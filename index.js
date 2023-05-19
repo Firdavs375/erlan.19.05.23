6cb444bd234682a5e806a56c492a004f11af3d40
Firdavs375 <firdavss.3755@gmail.com>
2023-05-19 20:25:06 +0500

    flex

---
+++ index.js
@@ -0,0 +1,50 @@
+// [1.Convert Minutes into Seconds](https://edabit.com/challenge/8q54MKnRrm89pSLmW)
+
+function convert(minutes) {
+	return minutes * 60
+}
+//
+
+
+// [2.Return the Sum of Two Numbers](https://edabit.com/challenge/3LpBLgNRyaHMvNb4j)
+
+function addition(a, b) {
+	return a + b
+}
+
+//
+
+
+//[3.Return the Next Number from the Integer Passed](https://edabit.com/challenge/NAQhEoxbofPidLxm9)
+function addition(num) {
+	return num + 1
+}
+//
+
+//[4.Convert Age to Days](https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw)
+function calcAge(age) {
+	return age * 365
+}
+//
+
+//[5.Power Calculator](https://edabit.com/challenge/wAdE9te55cowBLcPs)
+function circuitPower(voltage, current) {
+	return voltage * current
+}
+//
+
+//[6.Maximum Edge of a Triangle](https://edabit.com/challenge/nhXofMMyrowMyr9Nv)
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1
+}
+//
+
+//[7.Find the Perimeter of a Rectangle](https://edabit.com/challenge/XnJ24rWW7iJkNrtsh)
+function findPerimeter(length, width) {
+	return (length + width) * 2
+}
+//
+
+//[8.]()
+
+//
